7fcaf0a19a49f5eaa425ef3b5c823af115be7721
FeynmanLoo <feynman0919@gmail.com>
2017-06-08 14:59:51 +0800

    Signed-off-by: FeynmanLoo <feynman0919@gmail.com>

--- MingorProject/settings.py
+++ MingorProject/settings.py
@@ -78,17 +78,9 @@ WSGI_APPLICATION = 'MingorProject.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mingor',
-        'USER':'root',
-        'PASSWORD':'password',
-        'HOST':'www.51mingor.net',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
